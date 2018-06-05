2fb5b8db6f4a8d97f24e28aa753db00089a086bc
Anton <ant.nikitenko@gmail.com>
2018-06-04 22:57:32 -0700

    created account model, and basic view

---
+++ net-sql-banking-app/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using net_sql_banking_app.Models;
+
+namespace net_sql_banking_app.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Random()
+        {
+            var acc = new Account() { Name = "Chequing" };
+
+            return View(acc);
+        }
+    }
+}
---
+++ net-sql-banking-app/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace net_sql_banking_app.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+
+}
